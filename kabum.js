9e2e6b097533fc2b8321b2ace70b5180d3ac5462
ericafnunes <eriica.f.n@hotmail.com>
2021-06-08 19:55:36 -0300

    Programa de contagem regressiva

---
+++ kabum.js
@@ -0,0 +1,12 @@
+function solucao(numero) { 
+
+for(let indice = numero ; indice >= 0 ; indice--){
+  console.log(indice);
+ }
+  
+  console.log("KABUM");
+}    
+
+function processData(input) { 
+	solucao(parseInt(input, 10));
+} 
