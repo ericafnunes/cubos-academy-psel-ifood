0169ebcf428f903fe5d00d70a52bb1aea2bed14e
ericafnunes <eriica.f.n@hotmail.com>
2021-06-08 20:21:49 -0300

    Programa que verifica o tipo de filtro

---
+++ me-segue-no-insta.js
@@ -0,0 +1,16 @@
+function solucao(largura, altura) {git 
+
+      const foto = {
+        largura: 10,
+        altura: 5,
+    };
+
+      if(largura > altura){
+          console.log("PAISAGEM");
+      }else if(altura > largura){
+          console.log("RETRATO");
+      }else{
+          console.log("QUADRADA");
+      }
+
+  } 
